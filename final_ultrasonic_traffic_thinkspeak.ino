9fa9fd205171b880dc22497a0c27d5c2d56ce244
Akash Lonkar <akash.lonkar1@gmail.com>
2017-04-22 22:41:08 +0530

    Update final_ultrasonic_traffic_thinkspeak.ino

--- final_ultrasonic_traffic_thinkspeak.ino
+++ final_ultrasonic_traffic_thinkspeak.ino
@@ -22,7 +22,7 @@ int sensor2State=0;
 int sensor3State=0;
 int sensor4State=0;
 long duration, distance, UltraSensor1,UltraSensor2,UltraSensor3,UltraSensor4,UltraSensor5,UltraSensor6,UltraSensor7,UltraSensor8,road1,road2,road3,road4;
-
+int count1,count2,count3,count4,a,b,c,d;
 word case0 = 0b10010010;
 byte case1=  0b00010000;
 word case2 = 0b10010010;
@@ -45,7 +45,7 @@ byte case17 = 0b00100000;
 
 
 // replace with your channel's thingspeak API key
-String apiKey = "JF69RZRIF348P9CM";    //Api key of my channel Traffic management system
+String apiKey = "API_key";    //Api key of my channel Traffic management system
 
 // connect 10 to TX of Serial USB
 // connect 11 to RX of serial USB
@@ -143,7 +143,7 @@ void loop() {
   //Conditions for checking density of perticular road and release the signals
   
   if(road1<road2&&road1<road3&&road1<road4){
-    
+    count1++;
      // TCP connection
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += "184.106.153.149"; // api.thingspeak.com
@@ -195,6 +195,9 @@ void loop() {
    digitalWrite(latchPin, HIGH);
    delay(15000);
    
+    if(!(road1<road2&&road1<3&&road1<road4))
+    {
+      
     //for 4th yellow light 
    case2 = 0b10010010;
    case3 = 0b00100000;
@@ -203,9 +206,16 @@ void loop() {
    shiftOut(dataPin, clockPin, LSBFIRST, case2);
    digitalWrite(latchPin, HIGH);
    delay(8000);
-}
-else if (road2<road1&&road2<road3&&road2<road4){
+    }
+    if(count1>5)
+    {
+      count1=0;
+      routine();
+    }
   
+}
+ else if (road2<road1&&road2<road3&&road2<road4){
+  count2++;
    // TCP connection
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += "184.106.153.149"; // api.thingspeak.com
@@ -257,6 +267,8 @@ else if (road2<road1&&road2<road3&&road2<road4){
    digitalWrite(latchPin, HIGH);
    delay(10000);
 
+  if(!(road2<road1&&road2<road3&&road2<road4)){
+    
    //for 3rd yellow light
    case6 = 0b10010001;
    case7 = 0b01000000;
@@ -265,11 +277,18 @@ else if (road2<road1&&road2<road3&&road2<road4){
    shiftOut(dataPin, clockPin, LSBFIRST, case6);
    digitalWrite(latchPin, HIGH);
    delay(8000);
+  }
+   
+   if(count2>5)
+    {
+      count2=0;
+      routine();
+    }
  
 }
 
 else if(road3<road1&&road3<road2&&road3<road4){
-  
+  count3++;
    // TCP connection
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += "184.106.153.149"; // api.thingspeak.com
@@ -320,7 +339,9 @@ else if(road3<road1&&road3<road2&&road3<road4){
    shiftOut(dataPin, clockPin, LSBFIRST, case8);
    digitalWrite(latchPin, HIGH);
    delay(10000);
-
+  
+  if(!(road3<road1&&road3<road2&&road3<<road4)){
+    
    //for 2nd yellow light
     case10 = 0b10001010;
     case11 = 0b01000000;
@@ -331,9 +352,15 @@ else if(road3<road1&&road3<road2&&road3<road4){
    delay(5000);
 
 }
+  if(count3>5)
+    {
+      count3=0;
+      routine();
+    }
+}
 
 else if (road4<road1&&road4<road2&&road4<road3){
-  
+  count4++;
    // TCP connection
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += "184.106.153.149"; // api.thingspeak.com
@@ -385,7 +412,9 @@ else if (road4<road1&&road4<road2&&road4<road3){
    digitalWrite(latchPin, HIGH);
    delay(1000);
 
-   //for 1st yellow light 
+  if(!(road4<road1&&road4<road2&&road4<road3))
+  {
+    //for 1st yellow light 
    case14 = 0b01010010;
    case15 = 0b01000000;
    digitalWrite(latchPin, LOW);
@@ -393,7 +422,12 @@ else if (road4<road1&&road4<road2&&road4<road3){
    shiftOut(dataPin, clockPin, LSBFIRST, case14);
    digitalWrite(latchPin, HIGH);
    delay(1000);
-  
+}
+  if(count4>5)
+    {
+      count4=0;
+      routine();
+    }
 }
 else {
   
@@ -413,13 +447,13 @@ else {
   String getStr = "GET /update?api_key=";
   getStr += apiKey;
     
-    getStr +="&field1=";
+  getStr +="&field1=";
   getStr += String(state1);
   getStr +="&field2=";
   getStr += String(state2);
   getStr +="&field3=";
   getStr += String(state3);
-   getStr +="&field4=";
+  getStr +="&field4=";
   getStr += String(state4);
   getStr += "\r\n\r\n";
 
@@ -535,3 +569,79 @@ duration = pulseIn(echoPin, HIGH);
 distance = duration*0.034/2;
 delay(100);
 } 
+
+void routine()
+{
+  //for 4th green light
+   case1 = 0b10010010;
+   case0=  0b00010000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case1); 
+   shiftOut(dataPin, clockPin, LSBFIRST, case0);
+   digitalWrite(latchPin, HIGH);
+   delay(5000);
+   
+    //for 4th yellow light 
+   case2 = 0b10010010;
+   case3 = 0b00100000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case3);
+   shiftOut(dataPin, clockPin, LSBFIRST, case2);
+   digitalWrite(latchPin, HIGH);
+   delay(3000);
+  
+  //For 3rd green light
+   case4 = 0b10010000;
+   case5 = 0b11000000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case5);
+   shiftOut(dataPin, clockPin, LSBFIRST, case4);
+   digitalWrite(latchPin, HIGH);
+   delay(5000);
+
+   //for 3rd yellow light
+   case6 = 0b10010001;
+   case7 = 0b01000000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case7);
+   shiftOut(dataPin, clockPin, LSBFIRST, case6);
+   digitalWrite(latchPin, HIGH);
+   delay(3000);
+  
+  //for 2nd green light 
+    case8 = 0b10000110;
+    case9 = 0b01000000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case9);
+   shiftOut(dataPin, clockPin, LSBFIRST, case8);
+   digitalWrite(latchPin, HIGH);
+   delay(5000);
+
+   //for 2nd yellow light
+    case10 = 0b10001010;
+    case11 = 0b01000000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case11);
+   shiftOut(dataPin, clockPin, LSBFIRST, case12);
+   digitalWrite(latchPin, HIGH);
+   delay(3000);
+  
+  //for 1st greeen light 
+    case12 = 0b00110010;
+    case13 = 0b01000000; 
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case13);
+   shiftOut(dataPin, clockPin, LSBFIRST, case12);
+   digitalWrite(latchPin, HIGH);
+   delay(5000);
+
+   //for 1st yellow light 
+   case14 = 0b01010010;
+   case15 = 0b01000000;
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, case15);
+   shiftOut(dataPin, clockPin, LSBFIRST, case14);
+   digitalWrite(latchPin, HIGH);
+   delay(3000);
+  
+}
